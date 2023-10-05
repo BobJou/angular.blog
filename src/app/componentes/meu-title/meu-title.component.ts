9ba6906c64b1aaa10cfe27ab44b0b445e0af5298
BobJou <rogerjunio333@gmail.com>
2023-10-05 19:09:51 -0300

    estilizado

--- src/app/componentes/meu-title/meu-title.component.ts
+++ src/app/componentes/meu-title/meu-title.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-meu-title',
   templateUrl: './meu-title.component.html',
-  styleUrls: ['./meu-title.component.css']
+  styleUrls: ['./meu-title.component.css','./meu-title.responsive.component.css']
 })
 export class MeuTitleComponent {
-
+  @Input() menuTitulo:string =""
 }
